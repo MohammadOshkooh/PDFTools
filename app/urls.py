798ae73eaed7fc2d9447a6d09778fda7c3a802e7
Mohammad Oshkooh <mohammadoshkooh@gmail.com>
2022-07-25 00:36:17 +0430

    update merge

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import merge
 
 app_name = 'app'
 urlpatterns = [
-    path('', index, name='index')
+    path('', merge, name='merge')
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
 
 from app.forms import PDFForm
+from django.contrib import messages
 
 
-def index(request):
+def merge(request):
     if request.method == 'POST':
         upload_pdf_form = PDFForm(request.POST, request.FILES)
         if upload_pdf_form.is_valid():
@@ -15,20 +16,12 @@ def index(request):
                 out_file.append(pdf)
             with open('merged.pdf', 'wb') as output_stream:
                 out_file.write(output_stream)
-
-
-
-            # # split
-            # input_pdf = PdfFileReader(pdf)
-            # output_pdf = PdfFileWriter()
-            # for i in range(10, 20):
-            #     output_pdf.addPage(input_pdf.getPage(i))
-            # with open('file.pdf', 'wb') as out:
-            #     output_pdf.write(out)
+            messages.success(request, 'done successfully')
+            return redirect(reverse_lazy('app:merge'))
     else:
         upload_pdf_form = PDFForm()
 
     context = {
         'upload_pdf_form': upload_pdf_form
     }
-    return render(request, 'app/index.html', context)
+    return render(request, 'app/merge.html', context)
