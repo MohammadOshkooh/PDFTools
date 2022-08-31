1b51538452d6b72d66bd38b3a51f2e9afb1e13a1
mohammad oshkooh <100014582+MohammadOshkooh@users.noreply.github.com>
2022-08-31 16:36:55 +0430

    Update settings.py

--- project_config/settings.py
+++ project_config/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
 #         'PORT': env('MYSQL_DATABASE_PORT'),
 #     }
 # }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
